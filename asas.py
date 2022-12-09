337cb6f43ef05d97bcdbf9367c9e5e20adbaa1bc
Denys Bzenko <dbzenko@kse.org.ua>
2022-12-09 10:40:25 +0200

    initial code

---
+++ asas.py
@@ -0,0 +1,14 @@
+def task1(filename,country,year):
+    head = None
+    is_first_line = True
+    with open(filename,"r") as file:
+        for line in file.readlines():
+            if is_first_line:
+                head = line.split("\t")
+                is_first_line = False
+                continue
+
+            head.index("NOC")
+
+
+task1("data.csv", "USA", "1996")
